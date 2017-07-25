75847cf7a1423148b0a3ec4e25417c7feb348b6a
Igor <ashzak990@gmail.com>
2017-07-25 22:53:19 +0300

    Add files via upload

--- AppleShop/urls.py
+++ AppleShop/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-# from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib import admin
+
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^', include('shop.urls')),
+    url(r'^auth/', include('authentificate.urls')),
+    url(r'^registration/', include('registration.urls')),
+    url(r'^admin/', include('admin.urls')),
 ]
